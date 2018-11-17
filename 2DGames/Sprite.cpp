5015c9fae2a28aaef3e6c2a435cf06d932da61af
A12134 <35416592+A12134@users.noreply.github.com>
2018-11-17 17:16:39 +1100

    add in sprite class methods

--- 2DGames/Sprite.cpp
+++ 2DGames/Sprite.cpp
@@ -1,12 +1,116 @@
 #include "Sprite.h"
+#include <vector>
 
 
-
-Sprite::Sprite()
+Sprite::Sprite(std::string _name, Material* _mat, UV uv)
 {
+	this->mat = _mat;
+	this->spriteUV = uv;
+	setName(_name);
+
+	// this mesh is inaccessible from the resourceManager
+	sprite = new Mesh(_name + "_mesh");
+	// setup the sprite mesh
+	setupMesh();
+}
+
+Sprite::Sprite(std::string _name, Material* _mat, float sizeX, float sizeY, UV uv)
+{
+	this->mat = _mat;
+	this->spriteUV = uv;
+
+	setName(_name);
+
+	// this mesh is inaccessible from the resourceManager
+	sprite = new Mesh(_name + "_mesh");
+
+	// hard coded sprite size calculation
+	
+	// top right
+	vertices[0] = sizeX / 2;
+	vertices[1] = sizeY / 2;
+
+	// bottom right
+	vertices[3] = sizeX / 2;
+	vertices[4] = -sizeY / 2;
+
+	// bottom left
+	vertices[6] = -sizeX / 2;
+	vertices[7] = -sizeY / 2;
+
+	// top left
+	vertices[9] = -sizeX / 2;
+	vertices[10] = sizeY / 2;
+
+	// setup the sprite mesh
+	setupMesh();
+
 }
 
 
 Sprite::~Sprite()
 {
+}
+
+void Sprite::renderSprite(ShaderProgram * sp, glm::mat4 model, glm::mat4 view, glm::mat4 projection)
+{
+	sp->useThis();
+	mat->useThisMaterial();
+
+	glm::mat4 matrices = projection * view * model;
+
+	sp->setUniformMatrix4x4fv("transform", matrices, GL_FALSE);
+
+	glBindVertexArray(*(sprite->getVAOHandler()));
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
+	// reset material usage
+	glActiveTexture(GL_TEXTURE0);
+}
+
+void Sprite::setupMesh()
+{
+	// convert all data into a vector of Vertex data structure
+	std::vector<Vertex> vert;
+	Vertex v;
+	
+	// top right
+	v.mPosition = glm::vec3(vertices[0], vertices[1], vertices[2]);
+	v.mNormal = glm::vec3(0.0f);
+	v.mTexCoords = spriteUV.topRight;
+
+	vert.push_back(v);
+
+	// bottom right
+	v.mPosition = glm::vec3(vertices[3], vertices[4], vertices[5]);
+	v.mNormal = glm::vec3(0.0f);
+	v.mTexCoords = spriteUV.botRight;
+
+	vert.push_back(v);
+
+	// bottom left
+	v.mPosition = glm::vec3(vertices[6], vertices[7], vertices[8]);
+	v.mNormal = glm::vec3(0.0f);
+	v.mTexCoords = spriteUV.botLeft;
+
+	vert.push_back(v);
+
+	// top left
+	v.mPosition = glm::vec3(vertices[9], vertices[10], vertices[11]);
+	v.mNormal = glm::vec3(0.0f);
+	v.mTexCoords = spriteUV.topLeft;
+
+	vert.push_back(v);
+
+
+	// process indices
+	std::vector<unsigned int> _indices;
+
+	for (unsigned int i = 0; i < 6; i++)
+	{
+		_indices.push_back(indices[i]);
+	}
+
+	Utils::setupMesh(sprite, vert, _indices);
 }
--- 2DGames/Sprite.h
+++ 2DGames/Sprite.h
@@ -1,10 +1,61 @@
 #ifndef SPRITE_H
 #define SPRITE_H
+
+#include "GLFW/glfw3.h"
+#include "glad/glad.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "ResourceManager.h"
+#include "Utils.h"
+#include "Camera.h"
+
+struct UV 
+{
+	glm::vec2 topLeft;
+	glm::vec2 topRight;
+	glm::vec2 botLeft;
+	glm::vec2 botRight;
+};
+
+float vertices[] = {
+	0.5f, 0.5f, 0.0f,  // top right
+	0.5f, -0.5f, 0.0f, // bottom right
+	-0.5f, -0.5f, 0.0f, // bottom left
+	-0.5f, 0.5f, 0.0f // top left
+};
+
+unsigned int indices[] = 
+{
+	0, 1, 3,
+	1, 2, 3
+};
+
+/*
+TODO: custom size sprite
+the sprite is a rectangler that has two parameters: size x and size y
+*/
 class Sprite
 {
+private:
+	std::string name;
+	Material * mat;
+	UV spriteUV;
+	Mesh* sprite;
+
 public:
-	Sprite();
+	// default constructor, a square with size 1.0f and 1.0f
+	Sprite(std::string name, Material* _mat, UV uv);
+	Sprite(std::string name, Material* _mat, float sizeX, float sizeY, UV uv);
 	~Sprite();
+
+	std::string getName() { return name; }
+	void setName(std::string name) { this->name = name; }
+
+	void renderSprite(ShaderProgram* sp, glm::mat4 model, glm::mat4 view, glm::mat4 projection);
+
+private:
+	void setupMesh();
 };
 #endif
 
